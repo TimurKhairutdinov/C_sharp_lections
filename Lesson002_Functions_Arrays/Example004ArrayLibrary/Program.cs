ea7d33906d35d16c3f2aa38b6f9931e4dd535bd3
Timur <t1m.1h@yandex.ru>
2022-05-01 00:30:55 +1200

    add Example 004

---
+++ Lesson002_Functions_Arrays/Example004ArrayLibrary/Program.cs
@@ -0,0 +1,29 @@
+﻿void FillArray(int[] collection) // Метод, который заполняет массив случайными числами.
+{
+    int length = collection.Length; // Назначение переменной из размера массива.
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(0,99); // Заполнение массива случайным числом.
+        index++;
+        
+    }
+}
+
+void PrintArray(int[]col)
+{
+    int size = col.Length;
+    int position = 0;
+    while (position < size)
+    {
+        Console.Write(col[position] + ", ");
+        position++;
+    }
+}
+
+
+int[] array = new int[10]; // Создать новый массив, в котором будет 10 элементов. По умолчанию он заполнится нулями (0).
+
+FillArray(array);
+PrintArray(array);
+
