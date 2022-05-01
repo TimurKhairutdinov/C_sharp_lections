b42347f001b8c18671752c84c534937b2c0b774a
Timur <t1m.1h@yandex.ru>
2022-05-01 21:20:48 +1200

    add Example 004 Array Library

--- Lesson002_Functions_Arrays/Example004ArrayLibrary/Program.cs
+++ Lesson002_Functions_Arrays/Example004ArrayLibrary/Program.cs
@@ -4,14 +4,14 @@
     int index = 0;
     while (index < length)
     {
-        collection[index] = new Random().Next(0,99); // Заполнение массива случайным числом.
+        collection[index] = new Random().Next(1,5); // Заполнение массива случайным числом.
         index++;
         
     }
 }
 
-void PrintArray(int[]col)
-{
+void PrintArray(int[]col) // Метод, который печатает массив.
+{ 
     int size = col.Length;
     int position = 0;
     while (position < size)
@@ -21,9 +21,33 @@ void PrintArray(int[]col)
     }
 }
 
+int indexOf(int[] collec, int find) // Метод поиска позиции в массиве по заданному аргументу.
+{
+    int count = collec.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collec[index]==find)
+        {
+            position = index;  
+            break; 
+        }
+        index++;
+    }
+    return position; 
+   
+}
 
 int[] array = new int[10]; // Создать новый массив, в котором будет 10 элементов. По умолчанию он заполнится нулями (0).
+
+Console.WriteLine("Введите число: ");
+int find = Convert.ToInt32(Console.ReadLine()); // Переменная аргумент для поиска.
 
 FillArray(array);
 PrintArray(array);
 
+Console.WriteLine();
+
+int pos = indexOf(array, find );
+Console.WriteLine(pos);
