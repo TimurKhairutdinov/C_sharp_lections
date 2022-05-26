0b4bf41d1dfd3687852370782fb706f15a0d760a
Timur.Khairutdinov <t1m.1h@yandex.ru>
2022-05-26 19:27:43 +0300

    add Lesson 004 Two Dimensional Arrays & Recursion, add example 001

--- Lesson004TwoDimensionalArraysAndRecursion/Example001TwoDimensionalArrays/Program.cs
+++ Lesson004TwoDimensionalArraysAndRecursion/Example001TwoDimensionalArrays/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Двумерные массивы.
+
+string[,] table = new string [2,5];
+table[1,2] = "слово";
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"- {table[rows,columns]} -");
+    }
+}
+
+int[,] matrix = new int[3, 4];
+RandomFillArray(matrix);
+WriteArray(matrix);
+
+void RandomFillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++) // цикл отвечающий за строки
+    {
+        for (int j = 0; j < array.GetLength(1); j++) // цикл отвечающий за столбцы
+        {
+            array[i,j] = new Random().Next(1,10); // 1 - включительно, 10 исключительно.
+        }
+    }
+}
+void WriteArray(int[,]array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
